cde8f7ec96d6a2150060818feb8e61503ef7a705
Victor Phan <victor.phann@gmail.com>
2020-08-31 03:50:56 -0700

    added ability to exit program and completed functionality

--- sniffer.py
+++ sniffer.py
@@ -28,28 +28,35 @@ import time
 import _thread
 from collections import Counter
 
+# Constants
 PRINT_DELAY_SEC = 10
 ETH_HEADER_LEN = 14
 IP_HEADER_LEN_MIN = 20
 TCP_HEADER_LEN_MIN = 20
 UDP_HEADER_LEN = 8
-
 ETH_UNPACK_FORMAT = "!6s6sH"
 IP_UNPACK_FORMAT = "!BBHHHBBH4s4s"
 TCP_UNPACK_FORMAT = "!HHLLBBHHH"
 UDP_UNPACK_FORMAT = "!HHHH"
-
 IPV4_NUMBER = 8
 TCP_NUMBER = 6
 UDP_NUMBER = 17
+protocol_table = {
+    num: name[8:] for name, num in vars(socket).items() if name.startswith("IPPROTO")
+}
 
-UDP_PACKET_LIST = []
-TCP_PACKET_LIST = []
-OTHER_PACKET_LIST = []
+
+def get_protocol_name_by_num(p_num):
+    try:
+        name = protocol_table[p_num]
+    except Exception:
+        name = "UNKNOWN"
+    return name
+
 
 def print_packet_list(title, p_list):
     print(title)
-    if(len(p_list) == 0):
+    if len(p_list) == 0:
         print("None")
     else:
         for item in p_list.keys():
@@ -57,83 +64,133 @@ def print_packet_list(title, p_list):
 
 
 def print_received_packet_info(delay):
-    print("Format: \nSource:port -> Destination:port => num_packets\n")
+    print("Format: \nProtocol Source:port->Destination:port => num_packets\n")
     while True:
         time.sleep(delay)
-        udp_counter = Counter(UDP_PACKET_LIST)
-        tcp_counter = Counter(TCP_PACKET_LIST)
-        other_counter = Counter(OTHER_PACKET_LIST)
-        UDP_PACKET_LIST.clear()
-        TCP_PACKET_LIST.clear()
-        OTHER_PACKET_LIST.clear()
-        print_packet_list("UDP Packets:", udp_counter)
-        print_packet_list("TCP Packets:", tcp_counter)
-        print_packet_list("OTHER Packets:", other_counter)
+        packet_list_lock.acquire()
+        packet_counter = Counter(PACKET_LIST)
+        PACKET_LIST.clear()
+        packet_list_lock.release()
+        print_packet_list("Packets:", packet_counter)
 
-try:
-    if len(sys.argv) != 2:
-        print("Please enter an interface to sniff packets on")
+
+def run_sniffer():
+    global s
+    try:
+        if len(sys.argv) != 2:
+            print("Please enter an interface to sniff packets on")
+            sys.exit()
+
+        # socket.ntohs(0x0003) allows to capture from tcp, udp, etc..
+        socket_lock.acquire()
+        s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+        s.bind((sys.argv[1], 0))
+        socket_lock.release()
+
+    except socket.error as err:
+        print("Socket could not be created. Error Code :", err)
+        sys.exit()
+    except Exception as err:
+        print("Error: ", err)
         sys.exit()
 
-    # socket.ntohs(0x0003) allows to capture from tcp, udp, etc..
-    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
-    s.bind((sys.argv[1], 0))
+    # Create a thread to print out number of incoming packets
+    try:
+        _thread.start_new_thread(print_received_packet_info, (PRINT_DELAY_SEC,))
+    except Exception as err:
+        print("Error unable to start thread:", err)
+        sys.exit()
 
-except socket.error as err:
-    print("Socket could not be created. Error Code :", err)
-    sys.exit()
-except Exception as err:
-    print("Error: ", err)
-    sys.exit()
+    try:
+        while True:
+            # packet_data, address = s.recvfrom(65565)
+            packet_data = s.recvfrom(65565)[0]
 
-try:
-    _thread.start_new_thread(print_received_packet_info, (PRINT_DELAY_SEC,))
-except Exception as err:
-    print("Error unable to start thread:", err)
-    
-while True:
-    packet_data, address = s.recvfrom(65565)
+            # unpack the ethernet portion
+            eth_header = struct.unpack(ETH_UNPACK_FORMAT, packet_data[0:ETH_HEADER_LEN])
 
-    # unpack the ethernet portion
-    eth_header = struct.unpack(ETH_UNPACK_FORMAT, packet_data[0:ETH_HEADER_LEN])
+            eth_type = socket.ntohs(eth_header[2])
+            if eth_type != IPV4_NUMBER:
+                continue
 
-    # 8 means ipv4
-    eth_type = socket.ntohs(eth_header[2])
+            # unpack ip portion
+            ip_header = struct.unpack(
+                IP_UNPACK_FORMAT,
+                packet_data[ETH_HEADER_LEN : ETH_HEADER_LEN + IP_HEADER_LEN_MIN],
+            )
+            # version is first 4 bits so if we bit shift 4 bits we ge the value
+            # version = ip_header[0] >> 4
+            # head len is next 4 bits so if we & with 0b00001111 we will get len
+            ip_header_len = (ip_header[0] & 0xF) * 4
 
-    if eth_type != IPV4_NUMBER:
-        continue
+            protocol_num = ip_header[6]
+            protocol_str = get_protocol_name_by_num(protocol_num)
+            source_addr = socket.inet_ntoa(ip_header[8])
+            dest_addr = socket.inet_ntoa(ip_header[9])
 
-    # unpack ip portion
-    ip_header = struct.unpack(
-        IP_UNPACK_FORMAT,
-        packet_data[ETH_HEADER_LEN : ETH_HEADER_LEN + IP_HEADER_LEN_MIN],
+            if protocol_num == TCP_NUMBER:
+                # unpack tcp portion
+                tcp_header_index = ETH_HEADER_LEN + ip_header_len
+                tcp_header = struct.unpack(
+                    TCP_UNPACK_FORMAT,
+                    packet_data[
+                        tcp_header_index : tcp_header_index + TCP_HEADER_LEN_MIN
+                    ],
+                )
+                source_port = tcp_header[0]
+                dest_port = tcp_header[1]
+            elif protocol_num == UDP_NUMBER:
+                # unpack udp portion
+                udp_header_index = ETH_HEADER_LEN + ip_header_len
+                udp_header = struct.unpack(
+                    UDP_UNPACK_FORMAT,
+                    packet_data[udp_header_index : udp_header_index + UDP_HEADER_LEN],
+                )
+                source_port = udp_header[0]
+                dest_port = udp_header[1]
+
+            # Filter data into respective lists
+            packet_list_lock.acquire()
+            if protocol_num == TCP_NUMBER or protocol_num == UDP_NUMBER:
+                PACKET_LIST.append(
+                    protocol_str
+                    + " "
+                    + str(source_addr)
+                    + ":"
+                    + str(source_port)
+                    + "->"
+                    + str(dest_addr)
+                    + ":"
+                    + str(dest_port)
+                )
+            else:
+                PACKET_LIST.append(
+                    protocol_str + " " + str(source_addr) + "->" + str(dest_addr)
+                )
+            packet_list_lock.release()
+    except Exception:
+        print("exit was received from main thread")
+
+
+def main():
+    # child threads are default daemon threads
+    # once non daemon threads are gone the program exits
+    _thread.start_new_thread(
+        run_sniffer,
     )
-    version = ip_header[0] >> 4
-    ip_header_len = (ip_header[0] & 0xF) * 4
-    protocol = ip_header[6]
-    source_addr = socket.inet_ntoa(ip_header[8])
-    dest_addr = socket.inet_ntoa(ip_header[9])
-    if (protocol == TCP_NUMBER):
-        tcp_header_index = ETH_HEADER_LEN + ip_header_len
-        tcp_header = struct.unpack(
-            TCP_UNPACK_FORMAT,
-            packet_data[tcp_header_index : tcp_header_index + TCP_HEADER_LEN_MIN],
-        )
-        source_port = tcp_header[0]
-        dest_port = tcp_header[1]
-    elif (protocol == UDP_NUMBER):
-        udp_header_index = ETH_HEADER_LEN + ip_header_len
-        udp_header = struct.unpack(
-            UDP_UNPACK_FORMAT,
-            packet_data[udp_header_index : udp_header_index + UDP_HEADER_LEN],
-        )
-        source_port = udp_header[0]
-        dest_port = udp_header[1]
-    
-    #Filter data into respective lists
-    if(protocol == TCP_NUMBER):
-        TCP_PACKET_LIST.append(str(source_addr) + ":" + str(source_port) + "->" + str(dest_addr) + ":" + str(dest_port))
-    elif(protocol == UDP_NUMBER):
-        UDP_PACKET_LIST.append(str(source_addr) + ":" + str(source_port) + "->" + str(dest_addr) + ":" + str(dest_port))
-    else:
-        OTHER_PACKET_LIST.append(str(source_addr) + "->" + str(dest_addr))
+    while input("To exit program type 'exit':\n") != "exit":
+        continue
+    try:
+        socket_lock.acquire()
+        socket.close(s.fileno())
+        socket_lock.release()
+    except Exception:
+        print("Socket already closed")
+    print("Exiting program")
+
+if __name__ == "__main__":
+    s = -1
+    PACKET_LIST = []
+    packet_list_lock = _thread.allocate_lock()
+    socket_lock = _thread.allocate_lock()
+    main()
